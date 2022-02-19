8869296be112c3e222a2030a44c88ca129ce7d57
AnkurDey76 <99506839+AnkurDey76@users.noreply.github.com>
2022-02-19 12:43:23 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,10 @@
+import phonenumbers
+from test import number
+
+from phonenumbers import geocoder
+ch_nmber= phonenumbers.parse(number, "CH")
+print(geocoder.description_for_number(ch_nmber, "en"))
+
+from phonenumbers import carrier
+service_nmber= phonenumbers.parse(number, "RO")
+print(carrier.name_for_number(service_nmber, "en"))
